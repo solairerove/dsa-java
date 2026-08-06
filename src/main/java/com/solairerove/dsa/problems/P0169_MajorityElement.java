d9a892e01740bb3cc53af88121ee185c0b89467a
solairerove <solairerove@gmail.com>
2026-08-06 12:39:57 +0700

    Implement Boyer-Moore algorithm for majority element detection with corresponding test cases

--- src/main/java/com/solairerove/dsa/problems/P0169_MajorityElement.java
+++ src/main/java/com/solairerove/dsa/problems/P0169_MajorityElement.java
@@ -10,4 +10,17 @@ public class P0169_MajorityElement {
 
         return nums[nums.length / 2];
     }
+
+    // time O(n), space O(1)
+    public static int majorityElementBoyerMoore(int[] nums) {
+        int res = 0, cnt = 0;
+        for (int num : nums) {
+            if (cnt == 0) {
+                res = num;
+            }
+            cnt += (num == res) ? 1 : -1;
+        }
+
+        return res;
+    }
 }
--- src/test/java/com/solairerove/dsa/problems/P0169_MajorityElementTest.java
+++ src/test/java/com/solairerove/dsa/problems/P0169_MajorityElementTest.java
@@ -36,4 +36,34 @@ class P0169_MajorityElementTest {
     void handlesNegativeNumbers() {
         assertEquals(-5, P0169_MajorityElement.majorityElement(new int[]{-5, -5, 3, -5, 8}));
     }
+
+    @Test
+    void boyerMooreFindsMajorityInSmallArray() {
+        assertEquals(3, P0169_MajorityElement.majorityElementBoyerMoore(new int[]{3, 2, 3}));
+    }
+
+    @Test
+    void boyerMooreFindsMajorityInUnsortedArray() {
+        assertEquals(2, P0169_MajorityElement.majorityElementBoyerMoore(new int[]{2, 2, 1, 1, 1, 2, 2}));
+    }
+
+    @Test
+    void boyerMooreHandlesSingleElement() {
+        assertEquals(1, P0169_MajorityElement.majorityElementBoyerMoore(new int[]{1}));
+    }
+
+    @Test
+    void boyerMooreHandlesAllSameElements() {
+        assertEquals(7, P0169_MajorityElement.majorityElementBoyerMoore(new int[]{7, 7, 7, 7}));
+    }
+
+    @Test
+    void boyerMooreHandlesEvenLengthWithExactHalfPlusOne() {
+        assertEquals(4, P0169_MajorityElement.majorityElementBoyerMoore(new int[]{4, 1, 4, 4}));
+    }
+
+    @Test
+    void boyerMooreHandlesNegativeNumbers() {
+        assertEquals(-5, P0169_MajorityElement.majorityElementBoyerMoore(new int[]{-5, -5, 3, -5, 8}));
+    }
 }
