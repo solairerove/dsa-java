b8b1620d018f5fd013aaf06c6f51817fdc36ad5b
solairerove <solairerove@gmail.com>
2026-08-07 14:53:12 +0700

    Refactor `P0347_TopKFrequentElements`: streamline frequency map population and improve variable naming

--- src/main/java/com/solairerove/dsa/problems/P0347_TopKFrequentElements.java
+++ src/main/java/com/solairerove/dsa/problems/P0347_TopKFrequentElements.java
@@ -15,26 +15,21 @@ public class P0347_TopKFrequentElements {
         // add in to res [], and continue to iterate again filtering freq by res
         Map<Integer, Integer> freq = new HashMap<>();
         for (int num : nums) {
-            if (!freq.containsKey(num)) {
-                freq.put(num, 0);
-            }
-
-            freq.put(num, freq.get(num) + 1);
+            freq.merge(num, 1, Integer::sum);
         }
 
-        Set<Integer> set = new HashSet<>();
+        Set<Integer> seen = new HashSet<>();
         int[] res = new int[k];
         for (int j = 0; j < k; j++) {
             int maxV = 0;
             int maxK = nums[0];
             for (Map.Entry<Integer, Integer> e : freq.entrySet()) {
-                if (!set.contains(e.getKey()) && e.getValue() > maxV) {
+                if (!seen.contains(e.getKey()) && e.getValue() > maxV) {
                     maxV = e.getValue();
                     maxK = e.getKey();
                 }
             }
-            if (!set.contains(maxK)) {
-                set.add(maxK);
+            if (seen.add(maxK)) {
                 res[j] = maxK;
             }
         }
