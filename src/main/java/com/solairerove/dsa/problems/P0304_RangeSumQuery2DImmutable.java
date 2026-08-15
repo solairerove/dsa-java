55b8b74b4cf52d6e980674f001a3c2b6038c3899
solairerove <solairerove@gmail.com>
2026-08-15 13:53:49 +0700

    Refactor `RangeSumQuery2DImmutable` to improve variable naming and add comprehensive and edge case test coverage

--- src/main/java/com/solairerove/dsa/problems/P0304_RangeSumQuery2DImmutable.java
+++ src/main/java/com/solairerove/dsa/problems/P0304_RangeSumQuery2DImmutable.java
@@ -2,17 +2,16 @@ package com.solairerove.dsa.problems;
 
 public class P0304_RangeSumQuery2DImmutable {
 
-    final int[][] ps;
+    int[][] ps;
 
     public P0304_RangeSumQuery2DImmutable(int[][] matrix) {
         int rows = matrix.length, cols = matrix[0].length;
         ps = new int[rows + 1][cols + 1];
         for (int r = 0; r < rows; r++) {
-            int prefix = 0;
+            int currentRowPrefix = 0;
             for (int c = 0; c < cols; c++) {
-                prefix += matrix[r][c];
-                int above = ps[r][c + 1];
-                ps[r + 1][c + 1] = prefix + above;
+                currentRowPrefix += matrix[r][c];
+                ps[r + 1][c + 1] = currentRowPrefix + ps[r][c + 1];
             }
         }
     }
@@ -24,10 +23,10 @@ public class P0304_RangeSumQuery2DImmutable {
         row2++;
         col2++;
         int bottomRight = ps[row2][col2];
-        int above = ps[row1 - 1][col2];
-        int left = ps[row2][col1 - 1];
+        int topRight = ps[row1 - 1][col2];
+        int bottomLeft = ps[row2][col1 - 1];
         int topLeft = ps[row1 - 1][col1 - 1];
 
-        return bottomRight - above - left + topLeft;
+        return bottomRight - topRight - bottomLeft + topLeft;
     }
 }
--- src/test/java/com/solairerove/dsa/problems/P0304_RangeSumQuery2DImmutableTest.java
+++ src/test/java/com/solairerove/dsa/problems/P0304_RangeSumQuery2DImmutableTest.java
@@ -9,110 +9,157 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SuppressWarnings("NewClassNamingConvention")
 class P0304_RangeSumQuery2DImmutableTest {
 
-    private static final int[][] EXAMPLE = {
-            {3, 0, 1, 4, 2},
-            {5, 6, 3, 2, 1},
-            {1, 2, 0, 1, 5},
-            {4, 1, 0, 1, 7},
-            {1, 0, 3, 0, 5}
-    };
-
     @Test
     void sumRegion_example1() {
-        P0304_RangeSumQuery2DImmutable obj = new P0304_RangeSumQuery2DImmutable(EXAMPLE);
+        P0304_RangeSumQuery2DImmutable obj = new P0304_RangeSumQuery2DImmutable(new int[][]{
+                {3, 0, 1, 4, 2},
+                {5, 6, 3, 2, 1},
+                {1, 2, 0, 1, 5},
+                {4, 1, 0, 1, 7},
+                {1, 0, 3, 0, 5}
+        });
         assertEquals(8, obj.sumRegion(2, 1, 4, 3));
         assertEquals(11, obj.sumRegion(1, 1, 2, 2));
         assertEquals(12, obj.sumRegion(1, 2, 2, 4));
     }
 
     @Test
-    void sumRegion_singleCell() {
-        P0304_RangeSumQuery2DImmutable obj = new P0304_RangeSumQuery2DImmutable(EXAMPLE);
-        assertEquals(3, obj.sumRegion(0, 0, 0, 0));
-        assertEquals(5, obj.sumRegion(4, 4, 4, 4));
-        assertEquals(6, obj.sumRegion(1, 1, 1, 1));
+    void sumRegion_singleCellMatrix() {
+        P0304_RangeSumQuery2DImmutable obj = new P0304_RangeSumQuery2DImmutable(new int[][]{{7}});
+        assertEquals(7, obj.sumRegion(0, 0, 0, 0));
+    }
+
+    @Test
+    void sumRegion_singleCellQueries() {
+        P0304_RangeSumQuery2DImmutable obj = new P0304_RangeSumQuery2DImmutable(new int[][]{
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        });
+        assertEquals(1, obj.sumRegion(0, 0, 0, 0));
+        assertEquals(5, obj.sumRegion(1, 1, 1, 1));
+        assertEquals(9, obj.sumRegion(2, 2, 2, 2));
     }
 
     @Test
     void sumRegion_wholeMatrix() {
-        P0304_RangeSumQuery2DImmutable obj = new P0304_RangeSumQuery2DImmutable(EXAMPLE);
-        assertEquals(58, obj.sumRegion(0, 0, 4, 4));
+        P0304_RangeSumQuery2DImmutable obj = new P0304_RangeSumQuery2DImmutable(new int[][]{
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        });
+        assertEquals(45, obj.sumRegion(0, 0, 2, 2));
     }
 
     @Test
-    void sumRegion_singleRow() {
-        P0304_RangeSumQuery2DImmutable obj = new P0304_RangeSumQuery2DImmutable(new int[][]{{1, 2, 3, 4}});
-        assertEquals(10, obj.sumRegion(0, 0, 0, 3));
-        assertEquals(5, obj.sumRegion(0, 1, 0, 2));
+    void sumRegion_singleRowMatrix() {
+        P0304_RangeSumQuery2DImmutable obj = new P0304_RangeSumQuery2DImmutable(new int[][]{{1, 2, 3, 4, 5}});
+        assertEquals(15, obj.sumRegion(0, 0, 0, 4));
+        assertEquals(9, obj.sumRegion(0, 1, 0, 3));
+        assertEquals(3, obj.sumRegion(0, 2, 0, 2));
     }
 
     @Test
-    void sumRegion_singleColumn() {
+    void sumRegion_singleColumnMatrix() {
         P0304_RangeSumQuery2DImmutable obj = new P0304_RangeSumQuery2DImmutable(new int[][]{{1}, {2}, {3}, {4}});
         assertEquals(10, obj.sumRegion(0, 0, 3, 0));
         assertEquals(5, obj.sumRegion(1, 0, 2, 0));
     }
 
     @Test
-    void sumRegion_oneByOneMatrix() {
-        P0304_RangeSumQuery2DImmutable obj = new P0304_RangeSumQuery2DImmutable(new int[][]{{-7}});
-        assertEquals(-7, obj.sumRegion(0, 0, 0, 0));
+    void sumRegion_fullRowAndFullColumn() {
+        P0304_RangeSumQuery2DImmutable obj = new P0304_RangeSumQuery2DImmutable(new int[][]{
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        });
+        assertEquals(15, obj.sumRegion(1, 0, 1, 2));
+        assertEquals(18, obj.sumRegion(0, 2, 2, 2));
     }
 
     @Test
-    void sumRegion_negativeValues() {
+    void sumRegion_allNegative() {
         P0304_RangeSumQuery2DImmutable obj = new P0304_RangeSumQuery2DImmutable(new int[][]{
-                {-1, -2, -3},
-                {-4, -5, -6},
-                {-7, -8, -9}
+                {-1, -2},
+                {-3, -4}
         });
-        assertEquals(-45, obj.sumRegion(0, 0, 2, 2));
-        assertEquals(-28, obj.sumRegion(1, 1, 2, 2));
-        assertEquals(-5, obj.sumRegion(1, 1, 1, 1));
+        assertEquals(-10, obj.sumRegion(0, 0, 1, 1));
+        assertEquals(-7, obj.sumRegion(1, 0, 1, 1));
+        assertEquals(-6, obj.sumRegion(0, 1, 1, 1));
     }
 
     @Test
-    void sumRegion_mixedSigns() {
+    void sumRegion_mixedSignsCancelToZero() {
         P0304_RangeSumQuery2DImmutable obj = new P0304_RangeSumQuery2DImmutable(new int[][]{
-                {1, -1, 2},
-                {-2, 3, -3},
-                {4, -4, 5}
+                {5, -5},
+                {-5, 5}
         });
-        assertEquals(5, obj.sumRegion(0, 0, 2, 2));
-        assertEquals(1, obj.sumRegion(0, 0, 1, 1));
-        assertEquals(1, obj.sumRegion(1, 1, 2, 2));
+        assertEquals(0, obj.sumRegion(0, 0, 1, 1));
+        assertEquals(0, obj.sumRegion(0, 0, 0, 1));
     }
 
     @Test
     void sumRegion_zeros() {
-        P0304_RangeSumQuery2DImmutable obj = new P0304_RangeSumQuery2DImmutable(new int[3][3]);
-        assertEquals(0, obj.sumRegion(0, 0, 2, 2));
-        assertEquals(0, obj.sumRegion(1, 1, 1, 1));
-    }
-
-    @Test
-    void sumRegion_repeatedQueriesAreStable() {
-        P0304_RangeSumQuery2DImmutable obj = new P0304_RangeSumQuery2DImmutable(EXAMPLE);
-        assertEquals(8, obj.sumRegion(2, 1, 4, 3));
-        assertEquals(8, obj.sumRegion(2, 1, 4, 3));
-        assertEquals(11, obj.sumRegion(1, 1, 2, 2));
+        P0304_RangeSumQuery2DImmutable obj = new P0304_RangeSumQuery2DImmutable(new int[][]{
+                {0, 0, 0},
+                {0, 0, 0}
+        });
+        assertEquals(0, obj.sumRegion(0, 0, 1, 2));
     }
 
     @Test
     void sumRegion_boundaryValues() {
         P0304_RangeSumQuery2DImmutable obj = new P0304_RangeSumQuery2DImmutable(new int[][]{
-                {-100000, 100000},
-                {100000, -100000}
+                {-10000, 10000},
+                {10000, -10000}
         });
         assertEquals(0, obj.sumRegion(0, 0, 1, 1));
-        assertEquals(0, obj.sumRegion(0, 0, 0, 1));
-        assertEquals(100000, obj.sumRegion(0, 1, 0, 1));
+        assertEquals(-10000, obj.sumRegion(0, 0, 0, 0));
+        assertEquals(10000, obj.sumRegion(0, 1, 0, 1));
+        assertEquals(0, obj.sumRegion(0, 0, 1, 0));
+    }
+
+    @Test
+    void sumRegion_repeatedQueriesAreStable() {
+        P0304_RangeSumQuery2DImmutable obj = new P0304_RangeSumQuery2DImmutable(new int[][]{
+                {2, 4},
+                {6, 8}
+        });
+        assertEquals(20, obj.sumRegion(0, 0, 1, 1));
+        assertEquals(20, obj.sumRegion(0, 0, 1, 1));
+        assertEquals(12, obj.sumRegion(0, 1, 1, 1));
+    }
+
+    @Test
+    void sumRegion_doesNotMutateInputMatrix() {
+        int[][] matrix = {
+                {1, 2},
+                {3, 4}
+        };
+        P0304_RangeSumQuery2DImmutable obj = new P0304_RangeSumQuery2DImmutable(matrix);
+        obj.sumRegion(0, 0, 1, 1);
+
+        assertEquals(1, matrix[0][0]);
+        assertEquals(2, matrix[0][1]);
+        assertEquals(3, matrix[1][0]);
+        assertEquals(4, matrix[1][1]);
+    }
+
+    @Test
+    void sumRegion_nonSquareMatrix() {
+        P0304_RangeSumQuery2DImmutable obj = new P0304_RangeSumQuery2DImmutable(new int[][]{
+                {1, 2, 3, 4},
+                {5, 6, 7, 8}
+        });
+        assertEquals(36, obj.sumRegion(0, 0, 1, 3));
+        assertEquals(18, obj.sumRegion(0, 1, 1, 2));
+        assertEquals(26, obj.sumRegion(1, 0, 1, 3));
     }
 
     @Test
     void sumRegion_matchesBruteForceOnRandomInput() {
         Random random = new Random(304);
-        int rows = 60, cols = 40;
+        int rows = 40, cols = 30;
         int[][] matrix = new int[rows][cols];
         for (int r = 0; r < rows; r++) {
             for (int c = 0; c < cols; c++) {
