3df7053f21ae6e85b268efdfa24bddad14b56c29
solairerove <solairerove@gmail.com>
2026-08-16 05:05:40 +0700

    Remove `productExceptSelfNaive` implementation and associated tests to simplify and streamline the codebase

--- src/main/java/com/solairerove/dsa/problems/P0238_ProductOfArrayExceptSelf.java
+++ src/main/java/com/solairerove/dsa/problems/P0238_ProductOfArrayExceptSelf.java
@@ -2,44 +2,6 @@ package com.solairerove.dsa.problems;
 
 public class P0238_ProductOfArrayExceptSelf {
 
-    // naive: two extra arrays
-    // time O(n), space O(n)
-    public static int[] productExceptSelfNaive(int[] nums) {
-        // [1, 2, 4, 6]
-        // keeping in head prefix product anyway, but write only excluding nums[i]
-        // [1,] ps=1
-        // [1,1] ps=2
-        // [1,1,2] ps=8
-        // [1,1,2,8] ps=36
-
-        // [1, 2, 4, 6]
-        // [1] ss=6
-        // [6,1] ss=24
-        // [24,6,1] ss=48
-        // [48,24,6,1] ss=48
-        int n = nums.length;
-        int ps = 1;
-        int[] prefixArr = new int[n];
-        for (int i = 0; i < n; i++) {
-            prefixArr[i] = ps;
-            ps *= nums[i];
-        }
-
-        int ss = 1;
-        int[] suffixArr = new int[n];
-        for (int i = n - 1; i >= 0; i--) {
-            suffixArr[i] = ss;
-            ss *= nums[i];
-        }
-
-        int[] res = new int[n];
-        for (int i = 0; i < n; i++) {
-            res[i] = prefixArr[i] * suffixArr[i];
-        }
-
-        return res;
-    }
-
     // time O(n), space O(1) excluding output
     public static int[] productExceptSelf(int[] nums) {
         int n = nums.length;
--- src/test/java/com/solairerove/dsa/problems/P0238_ProductOfArrayExceptSelfTest.java
+++ src/test/java/com/solairerove/dsa/problems/P0238_ProductOfArrayExceptSelfTest.java
@@ -5,73 +5,10 @@ import org.junit.jupiter.api.Test;
 import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SuppressWarnings("NewClassNamingConvention")
 class P0238_ProductOfArrayExceptSelfTest {
-
-    @Test
-    void productExceptSelfNaive_example1() {
-        assertArrayEquals(new int[]{24, 12, 8, 6}, P0238_ProductOfArrayExceptSelf.productExceptSelfNaive(new int[]{1, 2, 3, 4}));
-    }
-
-    @Test
-    void productExceptSelfNaive_example2() {
-        assertArrayEquals(new int[]{0, 0, 9, 0, 0}, P0238_ProductOfArrayExceptSelf.productExceptSelfNaive(new int[]{-1, 1, 0, -3, 3}));
-    }
-
-    @Test
-    void productExceptSelfNaive_commentWalkthrough() {
-        assertArrayEquals(new int[]{48, 24, 12, 8}, P0238_ProductOfArrayExceptSelf.productExceptSelfNaive(new int[]{1, 2, 4, 6}));
-    }
-
-    @Test
-    void productExceptSelfNaive_twoElements() {
-        assertArrayEquals(new int[]{7, 3}, P0238_ProductOfArrayExceptSelf.productExceptSelfNaive(new int[]{3, 7}));
-    }
-
-    @Test
-    void productExceptSelfNaive_singleZero() {
-        assertArrayEquals(new int[]{0, 0, 24, 0}, P0238_ProductOfArrayExceptSelf.productExceptSelfNaive(new int[]{2, 3, 0, 4}));
-    }
-
-    @Test
-    void productExceptSelfNaive_twoZeros() {
-        assertArrayEquals(new int[]{0, 0, 0, 0}, P0238_ProductOfArrayExceptSelf.productExceptSelfNaive(new int[]{2, 0, 0, 4}));
-    }
-
-    @Test
-    void productExceptSelfNaive_allNegative() {
-        assertArrayEquals(new int[]{-24, -12, -8, -6}, P0238_ProductOfArrayExceptSelf.productExceptSelfNaive(new int[]{-1, -2, -3, -4}));
-    }
-
-    @Test
-    void productExceptSelfNaive_mixedSigns() {
-        assertArrayEquals(new int[]{-6, 6, 3, -2}, P0238_ProductOfArrayExceptSelf.productExceptSelfNaive(new int[]{-1, 1, 2, -3}));
-    }
-
-    @Test
-    void productExceptSelfNaive_allOnes() {
-        assertArrayEquals(new int[]{1, 1, 1, 1}, P0238_ProductOfArrayExceptSelf.productExceptSelfNaive(new int[]{1, 1, 1, 1}));
-    }
-
-    @Test
-    void productExceptSelfNaive_doesNotMutateInput() {
-        int[] nums = {1, 2, 3, 4};
-
-        P0238_ProductOfArrayExceptSelf.productExceptSelfNaive(nums);
-
-        assertArrayEquals(new int[]{1, 2, 3, 4}, nums);
-    }
-
-    @Test
-    void productExceptSelfNaive_matchesBruteForceOnRandomInput() {
-        Random random = new Random(238);
-        for (int t = 0; t < 200; t++) {
-            int[] nums = randomArray(random);
-
-            assertArrayEquals(bruteForce(nums), P0238_ProductOfArrayExceptSelf.productExceptSelfNaive(nums));
-        }
-    }
 
     @Test
     void productExceptSelf_example1() {
@@ -127,7 +64,8 @@ class P0238_ProductOfArrayExceptSelfTest {
     void productExceptSelf_doesNotMutateInput() {
         int[] nums = {1, 2, 3, 4};
 
-        P0238_ProductOfArrayExceptSelf.productExceptSelf(nums);
+        var res = P0238_ProductOfArrayExceptSelf.productExceptSelf(nums);
+        assertNotNull(res);
 
         assertArrayEquals(new int[]{1, 2, 3, 4}, nums);
     }
@@ -139,18 +77,6 @@ class P0238_ProductOfArrayExceptSelfTest {
             int[] nums = randomArray(random);
 
             assertArrayEquals(bruteForce(nums), P0238_ProductOfArrayExceptSelf.productExceptSelf(nums));
-        }
-    }
-
-    @Test
-    void bothApproachesAgreeOnRandomInput() {
-        Random random = new Random(2380);
-        for (int t = 0; t < 200; t++) {
-            int[] nums = randomArray(random);
-
-            assertArrayEquals(
-                    P0238_ProductOfArrayExceptSelf.productExceptSelfNaive(nums),
-                    P0238_ProductOfArrayExceptSelf.productExceptSelf(nums));
         }
     }
 
