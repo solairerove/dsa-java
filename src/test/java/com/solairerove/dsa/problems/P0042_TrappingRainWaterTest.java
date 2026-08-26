e8ed1d67a801581234070374b6bc749538557ced
solairerove <solairerove@gmail.com>
2026-08-26 13:17:30 +0300

    Add `trap` implementation with O(n) time complexity and comprehensive test coverage

--- src/test/java/com/solairerove/dsa/problems/P0042_TrappingRainWaterTest.java
+++ src/test/java/com/solairerove/dsa/problems/P0042_TrappingRainWaterTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Random;
 
+import static com.solairerove.dsa.problems.P0042_TrappingRainWater.trap;
 import static com.solairerove.dsa.problems.P0042_TrappingRainWater.trapPrefixMax;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -79,6 +80,78 @@ class P0042_TrappingRainWaterTest {
                 height[i] = rnd.nextInt(10);
             }
             assertEquals(bruteForce(height), trapPrefixMax(height.clone()));
+        }
+    }
+
+    @Test
+    void shouldSolveExampleOneTwoPointer() {
+        assertEquals(6, trap(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}));
+    }
+
+    @Test
+    void shouldSolveExampleTwoTwoPointer() {
+        assertEquals(9, trap(new int[]{4, 2, 0, 3, 2, 5}));
+    }
+
+    @Test
+    void shouldHandleSingleBarTwoPointer() {
+        assertEquals(0, trap(new int[]{5}));
+    }
+
+    @Test
+    void shouldHandleTwoBarsTwoPointer() {
+        assertEquals(0, trap(new int[]{5, 5}));
+    }
+
+    @Test
+    void shouldHandleSimpleValleyTwoPointer() {
+        assertEquals(1, trap(new int[]{2, 1, 2}));
+    }
+
+    @Test
+    void shouldHandleStrictlyIncreasingTwoPointer() {
+        assertEquals(0, trap(new int[]{1, 2, 3, 4, 5}));
+    }
+
+    @Test
+    void shouldHandleStrictlyDecreasingTwoPointer() {
+        assertEquals(0, trap(new int[]{5, 4, 3, 2, 1}));
+    }
+
+    @Test
+    void shouldHandleFlatSurfaceTwoPointer() {
+        assertEquals(0, trap(new int[]{3, 3, 3, 3}));
+    }
+
+    @Test
+    void shouldHandleAllZerosTwoPointer() {
+        assertEquals(0, trap(new int[]{0, 0, 0}));
+    }
+
+    @Test
+    void shouldHandleDeepBasinTwoPointer() {
+        assertEquals(10, trap(new int[]{5, 0, 0, 5}));
+    }
+
+    @Test
+    void shouldHandleTwoSeparateBasinsTwoPointer() {
+        assertEquals(4, trap(new int[]{3, 1, 3, 1, 3}));
+    }
+
+    @Test
+    void shouldIgnoreWaterOutsideOuterWallsTwoPointer() {
+        assertEquals(1, trap(new int[]{1, 4, 3, 4, 1}));
+    }
+
+    @Test
+    void shouldMatchBruteForceOnRandomInputsTwoPointer() {
+        Random rnd = new Random(42);
+        for (int t = 0; t < 300; t++) {
+            int[] height = new int[1 + rnd.nextInt(30)];
+            for (int i = 0; i < height.length; i++) {
+                height[i] = rnd.nextInt(10);
+            }
+            assertEquals(bruteForce(height), trap(height.clone()));
         }
     }
 
