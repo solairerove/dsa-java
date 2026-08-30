f48a874c4d0d21af31a1b405337d5829aedd3a91
solairerove <solairerove@gmail.com>
2026-08-30 19:39:47 +0300

    Add `minSubArrayLen` implementation with O(n) time complexity and comprehensive test coverage

---
+++ src/main/java/com/solairerove/dsa/problems/P0209_MinimumSizeSubarraySum.java
@@ -0,0 +1,22 @@
+package com.solairerove.dsa.problems;
+
+public class P0209_MinimumSizeSubarraySum {
+
+    // time O(n), space O(1)
+    public static int minSubArrayLen(int target, int[] nums) {
+        int l = 0, r = 0;
+        int windowSum = 0;
+        int res = Integer.MAX_VALUE;
+        while (true) {
+            if (windowSum < target) {
+                if (r == nums.length) break;
+                windowSum += nums[r++];
+            } else {
+                res = Math.min(res, r - l);
+                windowSum -= nums[l++];
+            }
+        }
+
+        return res == Integer.MAX_VALUE ? 0 : res;
+    }
+}
---
+++ src/test/java/com/solairerove/dsa/problems/P0209_MinimumSizeSubarraySumTest.java
@@ -0,0 +1,70 @@
+package com.solairerove.dsa.problems;
+
+import org.junit.jupiter.api.Test;
+
+import static com.solairerove.dsa.problems.P0209_MinimumSizeSubarraySum.minSubArrayLen;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SuppressWarnings("NewClassNamingConvention")
+class P0209_MinimumSizeSubarraySumTest {
+
+    @Test
+    void basicExample() {
+        assertEquals(2, minSubArrayLen(7, new int[]{2, 3, 1, 2, 4, 3}));
+    }
+
+    @Test
+    void singleElementMeetsTarget() {
+        assertEquals(1, minSubArrayLen(4, new int[]{1, 4, 4}));
+    }
+
+    @Test
+    void noSubarrayReachesTarget() {
+        assertEquals(0, minSubArrayLen(11, new int[]{1, 1, 1, 1, 1, 1, 1, 1}));
+    }
+
+    @Test
+    void wholeArrayNeeded() {
+        assertEquals(5, minSubArrayLen(15, new int[]{1, 2, 3, 4, 5}));
+    }
+
+    @Test
+    void emptyArray() {
+        assertEquals(0, minSubArrayLen(1, new int[]{}));
+    }
+
+    @Test
+    void singleElementTooSmall() {
+        assertEquals(0, minSubArrayLen(5, new int[]{4}));
+    }
+
+    @Test
+    void singleElementExact() {
+        assertEquals(1, minSubArrayLen(5, new int[]{5}));
+    }
+
+    @Test
+    void minimalWindowAtEnd() {
+        assertEquals(1, minSubArrayLen(6, new int[]{1, 1, 1, 1, 7}));
+    }
+
+    @Test
+    void minimalWindowAtStart() {
+        assertEquals(1, minSubArrayLen(6, new int[]{7, 1, 1, 1, 1}));
+    }
+
+    @Test
+    void allEqualElements() {
+        assertEquals(3, minSubArrayLen(9, new int[]{3, 3, 3, 3}));
+    }
+
+    @Test
+    void largeValuesNoOverflow() {
+        assertEquals(2, minSubArrayLen(2000000000, new int[]{1000000000, 1000000000}));
+    }
+
+    @Test
+    void targetExceedsTotalSum() {
+        assertEquals(0, minSubArrayLen(100, new int[]{1, 2, 3}));
+    }
+}
