50b3c48f2153990ed2d8ce14490ff2aaa02a46cd
solairerove <solairerove@gmail.com>
2026-08-30 20:30:08 +0300

    Add `findClosestElementsBinarySearch` implementation with O(log(n - k) + k) time complexity and comprehensive test coverage

--- src/main/java/com/solairerove/dsa/problems/P0658_FindKClosestElements.java
+++ src/main/java/com/solairerove/dsa/problems/P0658_FindKClosestElements.java
@@ -18,4 +18,18 @@ public class P0658_FindKClosestElements {
 
         return res;
     }
+    // time O(log(n - k) + k), space O(k)
+    public static List<Integer> findClosestElementsBinarySearch(int[] arr, int k, int x) {
+        int l = 0, r = arr.length - k;
+        while (l < r) {
+            int m = l + (r - l) / 2;
+            if (x - arr[m] > arr[m + k] - x) l = m + 1;
+            else r = m;
+        }
+
+        List<Integer> res = new ArrayList<>();
+        for (int i = l; i < l + k; i++) res.add(arr[i]);
+
+        return res;
+    }
 }
--- src/test/java/com/solairerove/dsa/problems/P0658_FindKClosestElementsTest.java
+++ src/test/java/com/solairerove/dsa/problems/P0658_FindKClosestElementsTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static com.solairerove.dsa.problems.P0658_FindKClosestElements.findClosestElements;
+import static com.solairerove.dsa.problems.P0658_FindKClosestElements.findClosestElementsBinarySearch;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SuppressWarnings("NewClassNamingConvention")
@@ -73,5 +74,69 @@ class P0658_FindKClosestElementsTest {
     @Test
     void sparseGaps() {
         assertEquals(List.of(10, 20), findClosestElements(new int[]{10, 20, 40, 80}, 2, 12));
+    }
+    @Test
+    void binarySearch_xInsideRange() {
+        assertEquals(List.of(1, 2, 3, 4), findClosestElementsBinarySearch(new int[]{1, 2, 3, 4, 5}, 4, 3));
+    }
+
+    @Test
+    void binarySearch_xBelowRange() {
+        assertEquals(List.of(1, 2, 3, 4), findClosestElementsBinarySearch(new int[]{1, 2, 3, 4, 5}, 4, -1));
+    }
+
+    @Test
+    void binarySearch_xAboveRange() {
+        assertEquals(List.of(3, 4, 5), findClosestElementsBinarySearch(new int[]{1, 2, 3, 4, 5}, 3, 10));
+    }
+
+    @Test
+    void binarySearch_kEqualsLength() {
+        assertEquals(List.of(1, 2, 3, 4, 5), findClosestElementsBinarySearch(new int[]{1, 2, 3, 4, 5}, 5, 3));
+    }
+
+    @Test
+    void binarySearch_kIsOne() {
+        assertEquals(List.of(4), findClosestElementsBinarySearch(new int[]{1, 2, 3, 4, 5}, 1, 4));
+    }
+
+    @Test
+    void binarySearch_tiePrefersSmaller() {
+        assertEquals(List.of(1, 2), findClosestElementsBinarySearch(new int[]{1, 2, 3, 4}, 2, 2));
+    }
+
+    @Test
+    void binarySearch_tieExactMidpoint() {
+        assertEquals(List.of(2, 3), findClosestElementsBinarySearch(new int[]{1, 2, 3, 4}, 2, 3));
+    }
+
+    @Test
+    void binarySearch_duplicates() {
+        assertEquals(List.of(2, 2, 2), findClosestElementsBinarySearch(new int[]{1, 2, 2, 2, 3}, 3, 2));
+    }
+
+    @Test
+    void binarySearch_negativeValues() {
+        assertEquals(List.of(-3, -2, -1), findClosestElementsBinarySearch(new int[]{-5, -3, -2, -1, 4}, 3, -2));
+    }
+
+    @Test
+    void binarySearch_singleElementArray() {
+        assertEquals(List.of(7), findClosestElementsBinarySearch(new int[]{7}, 1, 100));
+    }
+
+    @Test
+    void binarySearch_xEqualsFirstElement() {
+        assertEquals(List.of(1, 2, 3), findClosestElementsBinarySearch(new int[]{1, 2, 3, 4, 5}, 3, 1));
+    }
+
+    @Test
+    void binarySearch_xEqualsLastElement() {
+        assertEquals(List.of(3, 4, 5), findClosestElementsBinarySearch(new int[]{1, 2, 3, 4, 5}, 3, 5));
+    }
+
+    @Test
+    void binarySearch_sparseGaps() {
+        assertEquals(List.of(10, 20), findClosestElementsBinarySearch(new int[]{10, 20, 40, 80}, 2, 12));
     }
 }
