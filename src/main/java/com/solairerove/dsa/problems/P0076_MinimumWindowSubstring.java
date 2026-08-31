0dee3fcae9362d072c3fc8fa07594afff5a92eea
solairerove <solairerove@gmail.com>
2026-08-31 19:45:26 +0300

    Add `minWindowMap` implementation with O(n + m) time complexity and comprehensive test coverage

--- src/main/java/com/solairerove/dsa/problems/P0076_MinimumWindowSubstring.java
+++ src/main/java/com/solairerove/dsa/problems/P0076_MinimumWindowSubstring.java
@@ -5,13 +5,42 @@ import java.util.Map;
 
 public class P0076_MinimumWindowSubstring {
 
-    // time O(n + m), space O(n + m)
+    // time O(n + m), space O(1)
     public static String minWindow(String s, String t) {
+        int[] freq = new int[128];
+        for (char ch : t.toCharArray()) freq[ch]++;
+
+        int l = 0, r = 0;
+        int required = t.length();
+        int i = 0, length = Integer.MAX_VALUE;
+        while (r < s.length()) {
+            if (freq[s.charAt(r)] > 0) required--;
+            freq[s.charAt(r)]--;
+
+            while (required == 0) {
+                if (r - l + 1 < length) {
+                    length = r - l + 1;
+                    i = l;
+                }
+
+                freq[s.charAt(l)]++;
+                if (freq[s.charAt(l)] > 0) required++;
+
+                l++;
+            }
+            r++;
+        }
+
+        return length == Integer.MAX_VALUE ? "" : s.substring(i, i + length);
+    }
+
+    // time O(n + m), space O(n + m)
+    public static String minWindowMap(String s, String t) {
         Map<Character, Integer> freq = new HashMap<>();
         Map<Character, Integer> window = new HashMap<>();
         for (char ch : t.toCharArray()) freq.merge(ch, 1, Integer::sum);
 
-        int cnt = 0;
+        int have = 0, need = freq.size();
         int l = 0, r = 0;
         int[] res = {-1, -1};
         int resLen = Integer.MAX_VALUE;
@@ -19,9 +48,9 @@ public class P0076_MinimumWindowSubstring {
             char ch = s.charAt(r);
             window.merge(ch, 1, Integer::sum);
 
-            if (freq.containsKey(ch) && freq.get(ch).equals(window.get(ch))) cnt++;
+            if (freq.containsKey(ch) && freq.get(ch).equals(window.get(ch))) have++;
 
-            while (cnt == freq.size()) {
+            while (have == need) {
                 if ((r - l + 1) < resLen) {
                     resLen = r - l + 1;
                     res[0] = l;
@@ -31,7 +60,7 @@ public class P0076_MinimumWindowSubstring {
                 char leftChar = s.charAt(l);
                 window.put(leftChar, window.get(leftChar) - 1);
                 if (freq.containsKey(leftChar) && window.get(leftChar) < freq.get(leftChar)) {
-                    cnt--;
+                    have--;
                 }
 
                 l++;
--- src/test/java/com/solairerove/dsa/problems/P0076_MinimumWindowSubstringTest.java
+++ src/test/java/com/solairerove/dsa/problems/P0076_MinimumWindowSubstringTest.java
@@ -3,6 +3,7 @@ package com.solairerove.dsa.problems;
 import org.junit.jupiter.api.Test;
 
 import static com.solairerove.dsa.problems.P0076_MinimumWindowSubstring.minWindow;
+import static com.solairerove.dsa.problems.P0076_MinimumWindowSubstring.minWindowMap;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SuppressWarnings("NewClassNamingConvention")
@@ -71,5 +72,70 @@ class P0076_MinimumWindowSubstringTest {
     @Test
     void shouldHandleAllSameChars() {
         assertEquals("aaa", minWindow("aaaaa", "aaa"));
+    }
+
+    @Test
+    void shouldFindWindowInExampleMap() {
+        assertEquals("BANC", minWindowMap("ADOBECODEBANC", "ABC"));
+    }
+
+    @Test
+    void shouldReturnWholeStringWhenEqualMap() {
+        assertEquals("a", minWindowMap("a", "a"));
+    }
+
+    @Test
+    void shouldReturnEmptyWhenTLongerThanSMap() {
+        assertEquals("", minWindowMap("a", "aa"));
+    }
+
+    @Test
+    void shouldHandleDuplicatesInTMap() {
+        assertEquals("aa", minWindowMap("aa", "aa"));
+    }
+
+    @Test
+    void shouldHandleExcessOfNeededCharMap() {
+        assertEquals("ab", minWindowMap("aab", "ab"));
+    }
+
+    @Test
+    void shouldPickLastShortestWindowMap() {
+        assertEquals("ba", minWindowMap("bba", "ab"));
+    }
+
+    @Test
+    void shouldReturnEmptyWhenCharMissingMap() {
+        assertEquals("", minWindowMap("abc", "d"));
+    }
+
+    @Test
+    void shouldReturnEmptyWhenSIsEmptyMap() {
+        assertEquals("", minWindowMap("", "a"));
+    }
+
+    @Test
+    void shouldBeCaseSensitiveMap() {
+        assertEquals("", minWindowMap("abc", "A"));
+    }
+
+    @Test
+    void shouldHandleRepeatedRequirementMap() {
+        assertEquals("abbc", minWindowMap("aabbcc", "abc"));
+    }
+
+    @Test
+    void shouldHandleScatteredCharsMap() {
+        assertEquals("cwae", minWindowMap("cabwefgewcwaefgcf", "cae"));
+    }
+
+    @Test
+    void shouldShrinkFromLeftMap() {
+        assertEquals("abc", minWindowMap("aaabc", "abc"));
+    }
+
+    @Test
+    void shouldHandleAllSameCharsMap() {
+        assertEquals("aaa", minWindowMap("aaaaa", "aaa"));
     }
 }
