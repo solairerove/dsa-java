4b4bc2ae838eb76fa1913ad467af9624381c4107
solairerove <solairerove@gmail.com>
2026-09-01 09:49:48 +0300

    Add `checkInclusionRequired` implementation with O(n) time complexity and comprehensive test coverage

--- src/main/java/com/solairerove/dsa/problems/P0567_PermutationInString.java
+++ src/main/java/com/solairerove/dsa/problems/P0567_PermutationInString.java
@@ -19,4 +19,36 @@ public class P0567_PermutationInString {
 
         return false;
     }
+
+    // time O(n), space O(1)
+    public static boolean checkInclusionRequired(String s1, String s2) {
+        int[] cnt = new int[26];
+        for (char ch : s1.toCharArray()) {
+            cnt[ch - 'a']++;
+        }
+
+        int required = s1.length();
+        int l = 0, r = 0;
+        while (r < s2.length()) {
+            int right = s2.charAt(r) - 'a';
+            if (cnt[right] > 0) required--;
+            cnt[right]--;
+
+            if (r - l + 1 > s1.length()) {
+                int left = s2.charAt(l) - 'a';
+                cnt[left]++;
+                if (cnt[left] > 0) required++;
+
+                l++;
+            }
+
+            if (required == 0) {
+                return true;
+            }
+
+            r++;
+        }
+
+        return false;
+    }
 }
--- src/test/java/com/solairerove/dsa/problems/P0567_PermutationInStringTest.java
+++ src/test/java/com/solairerove/dsa/problems/P0567_PermutationInStringTest.java
@@ -3,6 +3,7 @@ package com.solairerove.dsa.problems;
 import org.junit.jupiter.api.Test;
 
 import static com.solairerove.dsa.problems.P0567_PermutationInString.checkInclusion;
+import static com.solairerove.dsa.problems.P0567_PermutationInString.checkInclusionRequired;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -77,5 +78,75 @@ class P0567_PermutationInStringTest {
     @Test
     void longS2NoMatch() {
         assertFalse(checkInclusion("abc", "cccccbbbbbaaaaa"));
+    }
+
+    @Test
+    void permutationAtStartRequired() {
+        assertTrue(checkInclusionRequired("ab", "eidbaooo"));
+    }
+
+    @Test
+    void noPermutationRequired() {
+        assertFalse(checkInclusionRequired("ab", "eidboaoo"));
+    }
+
+    @Test
+    void exactMatchRequired() {
+        assertTrue(checkInclusionRequired("abc", "abc"));
+    }
+
+    @Test
+    void s1LongerThanS2Required() {
+        assertFalse(checkInclusionRequired("abcd", "abc"));
+    }
+
+    @Test
+    void singleCharPresentRequired() {
+        assertTrue(checkInclusionRequired("a", "a"));
+    }
+
+    @Test
+    void singleCharAbsentRequired() {
+        assertFalse(checkInclusionRequired("a", "b"));
+    }
+
+    @Test
+    void permutationAtEndRequired() {
+        assertTrue(checkInclusionRequired("adc", "dcda"));
+    }
+
+    @Test
+    void repeatedCharsNeedCorrectCountsRequired() {
+        assertFalse(checkInclusionRequired("aab", "abbab"));
+    }
+
+    @Test
+    void repeatedCharsMatchRequired() {
+        assertTrue(checkInclusionRequired("aab", "xyzaabq"));
+    }
+
+    @Test
+    void repeatedCharsMismatchOrderRequired() {
+        assertFalse(checkInclusionRequired("aab", "bbabb"));
+    }
+
+    @Test
+    void allSameCharsRequired() {
+        assertTrue(checkInclusionRequired("aaa", "aaaa"));
+    }
+
+    @Test
+    void notEnoughRepeatsRequired() {
+        assertFalse(checkInclusionRequired("aaa", "aabaa"));
+    }
+
+    @Test
+    void windowSlidesPastFalseStartRequired() {
+        assertTrue(checkInclusionRequired("hello", "ooolleohellx"));
+    }
+
+    @Test
+    void longS2NoMatchRequired() {
+        assertFalse(checkInclusionRequired("abc", "cccccbbbbbaaaaa"));
     }
 }
