1e82fd0b9be99b6f2338997e552151d5f7290d6b
solairerove <solairerove@gmail.com>
2026-09-06 16:12:20 +0300

    Add `asteroidCollisionInPlace` implementation with O(n) time complexity and comprehensive test coverage

--- src/main/java/com/solairerove/dsa/problems/P0735_AsteroidCollision.java
+++ src/main/java/com/solairerove/dsa/problems/P0735_AsteroidCollision.java
@@ -1,5 +1,6 @@
 package com.solairerove.dsa.problems;
 
+import java.util.Arrays;
 import java.util.Stack;
 
 public class P0735_AsteroidCollision {
@@ -25,5 +26,29 @@ public class P0735_AsteroidCollision {
         }
 
         return stack.stream().mapToInt(i -> i).toArray();
+    }
+
+    // time O(n), space O(1)
+    public static int[] asteroidCollisionInPlace(int[] asteroids) {
+        int i = -1;
+        for (int a : asteroids) {
+            while (i >= 0 && asteroids[i] > 0 && a < 0) {
+                if (asteroids[i] > Math.abs(a)) {
+                    a = 0;
+                    break;
+                } else if (asteroids[i] == Math.abs(a)) {
+                    i--;
+                    a = 0;
+                    break;
+                } else {
+                    i--;
+                }
+            }
+            if (a != 0) {
+                asteroids[++i] = a;
+            }
+        }
+
+        return Arrays.copyOfRange(asteroids, 0, i + 1);
     }
 }
--- src/test/java/com/solairerove/dsa/problems/P0735_AsteroidCollisionTest.java
+++ src/test/java/com/solairerove/dsa/problems/P0735_AsteroidCollisionTest.java
@@ -3,6 +3,7 @@ package com.solairerove.dsa.problems;
 import org.junit.jupiter.api.Test;
 
 import static com.solairerove.dsa.problems.P0735_AsteroidCollision.asteroidCollision;
+import static com.solairerove.dsa.problems.P0735_AsteroidCollision.asteroidCollisionInPlace;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 @SuppressWarnings("NewClassNamingConvention")
@@ -66,5 +67,64 @@ class P0735_AsteroidCollisionTest {
     @Test
     void mutualDestructionLeavesNeighbours() {
         assertArrayEquals(new int[]{-4, 4}, asteroidCollision(new int[]{-4, 5, -5, 4}));
+    }
+    @Test
+    void smallerAsteroidExplodesInPlace() {
+        assertArrayEquals(new int[]{5, 10}, asteroidCollisionInPlace(new int[]{5, 10, -5}));
+    }
+
+    @Test
+    void equalAsteroidsBothExplodeInPlace() {
+        assertArrayEquals(new int[]{}, asteroidCollisionInPlace(new int[]{8, -8}));
+    }
+
+    @Test
+    void chainOfCollisionsInPlace() {
+        assertArrayEquals(new int[]{10}, asteroidCollisionInPlace(new int[]{10, 2, -5}));
+    }
+
+    @Test
+    void noCollisionsWhenMovingApartInPlace() {
+        assertArrayEquals(new int[]{-2, -1, 1, 2}, asteroidCollisionInPlace(new int[]{-2, -1, 1, 2}));
+    }
+
+    @Test
+    void allMovingRightInPlace() {
+        assertArrayEquals(new int[]{1, 2, 3}, asteroidCollisionInPlace(new int[]{1, 2, 3}));
+    }
+
+    @Test
+    void allMovingLeftInPlace() {
+        assertArrayEquals(new int[]{-1, -2, -3}, asteroidCollisionInPlace(new int[]{-1, -2, -3}));
+    }
+
+    @Test
+    void singleAsteroidInPlace() {
+        assertArrayEquals(new int[]{42}, asteroidCollisionInPlace(new int[]{42}));
+    }
+
+    @Test
+    void emptyInputInPlace() {
+        assertArrayEquals(new int[]{}, asteroidCollisionInPlace(new int[]{}));
+    }
+
+    @Test
+    void bigLeftMoverWipesOutSeveralInPlace() {
+        assertArrayEquals(new int[]{-20}, asteroidCollisionInPlace(new int[]{1, 2, 3, -20}));
+    }
+
+    @Test
+    void leftMoverStoppedAfterWipingSomeInPlace() {
+        assertArrayEquals(new int[]{10}, asteroidCollisionInPlace(new int[]{10, 2, 3, -5}));
+    }
+
+    @Test
+    void survivorsOnBothSidesInPlace() {
+        assertArrayEquals(new int[]{-2, -1, -2, -2}, asteroidCollisionInPlace(new int[]{-2, -1, 1, -2, -2}));
+    }
+
+    @Test
+    void mutualDestructionLeavesNeighboursInPlace() {
+        assertArrayEquals(new int[]{-4, 4}, asteroidCollisionInPlace(new int[]{-4, 5, -5, 4}));
     }
 }
