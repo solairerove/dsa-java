49fe3c02e899cfe70392eaf1af64ca85d3954eff
solairerove <solairerove@gmail.com>
2026-09-07 18:26:44 +0300

    Add `maxVowelsFixedWindow` and `maxVowelsLookupTable` implementations with O(n) time complexity and comprehensive test coverage

--- src/main/java/com/solairerove/dsa/problems/P1456_MaximumNumberOfVowelsInASubstringOfGivenLength.java
+++ src/main/java/com/solairerove/dsa/problems/P1456_MaximumNumberOfVowelsInASubstringOfGivenLength.java
@@ -31,4 +31,54 @@ public class P1456_MaximumNumberOfVowelsInASubstringOfGivenLength {
 
         return res;
     }
+
+    // time O(n), space O(1)
+    public static int maxVowelsFixedWindow(String s, int k) {
+        Set<Character> vowels = new HashSet<>();
+        vowels.add('a');
+        vowels.add('e');
+        vowels.add('i');
+        vowels.add('o');
+        vowels.add('u');
+
+        int res = 0, cnt = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (vowels.contains(s.charAt(i))) cnt++;
+            if (i >= k && vowels.contains(s.charAt(i - k))) cnt--;
+
+            res = Math.max(res, cnt);
+        }
+
+        return res;
+    }
+
+    // time O(n), space O(1)
+    public static int maxVowelsLookupTable(String s, int k) {
+        int[] vowels = new int[128];
+        vowels['a']++;
+        vowels['e']++;
+        vowels['i']++;
+        vowels['o']++;
+        vowels['u']++;
+
+        char[] chars = s.toCharArray();
+        int max = 0;
+        for (int i = 0; i < k; i++) {
+            if (vowels[chars[i]] == 1) {
+                max++;
+            }
+        }
+        int temp = max;
+
+        for (int i = k; i < chars.length; i++) {
+            temp += vowels[chars[i]];
+            temp -= vowels[chars[i - k]];
+
+            if (max < temp) {
+                max = temp;
+            }
+        }
+
+        return max;
+    }
 }
--- src/test/java/com/solairerove/dsa/problems/P1456_MaximumNumberOfVowelsInASubstringOfGivenLengthTest.java
+++ src/test/java/com/solairerove/dsa/problems/P1456_MaximumNumberOfVowelsInASubstringOfGivenLengthTest.java
@@ -3,6 +3,8 @@ package com.solairerove.dsa.problems;
 import org.junit.jupiter.api.Test;
 
 import static com.solairerove.dsa.problems.P1456_MaximumNumberOfVowelsInASubstringOfGivenLength.maxVowels;
+import static com.solairerove.dsa.problems.P1456_MaximumNumberOfVowelsInASubstringOfGivenLength.maxVowelsFixedWindow;
+import static com.solairerove.dsa.problems.P1456_MaximumNumberOfVowelsInASubstringOfGivenLength.maxVowelsLookupTable;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SuppressWarnings("NewClassNamingConvention")
@@ -76,5 +78,143 @@ class P1456_MaximumNumberOfVowelsInASubstringOfGivenLengthTest {
     @Test
     void longRunOfVowelsAfterConsonants() {
         assertEquals(4, maxVowels("bbbbaaaabbbb", 5));
+    }
+    @Test
+    void fixedWindowLeetcodeExampleOne() {
+        assertEquals(3, maxVowelsFixedWindow("abciiidef", 3));
+    }
+
+    @Test
+    void fixedWindowLeetcodeExampleTwo() {
+        assertEquals(2, maxVowelsFixedWindow("aeiou", 2));
+    }
+
+    @Test
+    void fixedWindowLeetcodeExampleThree() {
+        assertEquals(2, maxVowelsFixedWindow("leetcode", 3));
+    }
+
+    @Test
+    void fixedWindowNoVowels() {
+        assertEquals(0, maxVowelsFixedWindow("rhythm", 4));
+    }
+
+    @Test
+    void fixedWindowAllVowelsWindowIsWholeString() {
+        assertEquals(5, maxVowelsFixedWindow("aeiou", 5));
+    }
+
+    @Test
+    void fixedWindowSingleCharVowel() {
+        assertEquals(1, maxVowelsFixedWindow("a", 1));
+    }
+
+    @Test
+    void fixedWindowSingleCharConsonant() {
+        assertEquals(0, maxVowelsFixedWindow("b", 1));
+    }
+
+    @Test
+    void fixedWindowWindowOfOne() {
+        assertEquals(1, maxVowelsFixedWindow("bcadef", 1));
+    }
+
+    @Test
+    void fixedWindowVowelsAtEnd() {
+        assertEquals(3, maxVowelsFixedWindow("bcdaei", 3));
+    }
+
+    @Test
+    void fixedWindowVowelsAtStart() {
+        assertEquals(3, maxVowelsFixedWindow("aeibcd", 3));
+    }
+
+    @Test
+    void fixedWindowBestWindowInMiddle() {
+        assertEquals(2, maxVowelsFixedWindow("xxaexx", 2));
+    }
+
+    @Test
+    void fixedWindowKEqualsStringLengthNoVowels() {
+        assertEquals(0, maxVowelsFixedWindow("xyz", 3));
+    }
+
+    @Test
+    void fixedWindowRepeatedPattern() {
+        assertEquals(2, maxVowelsFixedWindow("abababab", 4));
+    }
+
+    @Test
+    void fixedWindowLongRunOfVowelsAfterConsonants() {
+        assertEquals(4, maxVowelsFixedWindow("bbbbaaaabbbb", 5));
+    }
+    @Test
+    void lookupTableLeetcodeExampleOne() {
+        assertEquals(3, maxVowelsLookupTable("abciiidef", 3));
+    }
+
+    @Test
+    void lookupTableLeetcodeExampleTwo() {
+        assertEquals(2, maxVowelsLookupTable("aeiou", 2));
+    }
+
+    @Test
+    void lookupTableLeetcodeExampleThree() {
+        assertEquals(2, maxVowelsLookupTable("leetcode", 3));
+    }
+
+    @Test
+    void lookupTableNoVowels() {
+        assertEquals(0, maxVowelsLookupTable("rhythm", 4));
+    }
+
+    @Test
+    void lookupTableAllVowelsWindowIsWholeString() {
+        assertEquals(5, maxVowelsLookupTable("aeiou", 5));
+    }
+
+    @Test
+    void lookupTableSingleCharVowel() {
+        assertEquals(1, maxVowelsLookupTable("a", 1));
+    }
+
+    @Test
+    void lookupTableSingleCharConsonant() {
+        assertEquals(0, maxVowelsLookupTable("b", 1));
+    }
+
+    @Test
+    void lookupTableWindowOfOne() {
+        assertEquals(1, maxVowelsLookupTable("bcadef", 1));
+    }
+
+    @Test
+    void lookupTableVowelsAtEnd() {
+        assertEquals(3, maxVowelsLookupTable("bcdaei", 3));
+    }
+
+    @Test
+    void lookupTableVowelsAtStart() {
+        assertEquals(3, maxVowelsLookupTable("aeibcd", 3));
+    }
+
+    @Test
+    void lookupTableBestWindowInMiddle() {
+        assertEquals(2, maxVowelsLookupTable("xxaexx", 2));
+    }
+
+    @Test
+    void lookupTableKEqualsStringLengthNoVowels() {
+        assertEquals(0, maxVowelsLookupTable("xyz", 3));
+    }
+
+    @Test
+    void lookupTableRepeatedPattern() {
+        assertEquals(2, maxVowelsLookupTable("abababab", 4));
+    }
+
+    @Test
+    void lookupTableLongRunOfVowelsAfterConsonants() {
+        assertEquals(4, maxVowelsLookupTable("bbbbaaaabbbb", 5));
     }
 }
