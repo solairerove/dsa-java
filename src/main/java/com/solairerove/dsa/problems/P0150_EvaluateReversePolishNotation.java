816af239ecf6496f8ee5a33a7a6454df1d804235
solairerove <solairerove@gmail.com>
2026-09-08 10:20:51 +0300

    Add `evalRPNArray` implementation with O(n) time complexity and comprehensive test coverage

--- src/main/java/com/solairerove/dsa/problems/P0150_EvaluateReversePolishNotation.java
+++ src/main/java/com/solairerove/dsa/problems/P0150_EvaluateReversePolishNotation.java
@@ -34,4 +34,39 @@ public class P0150_EvaluateReversePolishNotation {
 
         return stack.peek();
     }
+
+    // time O(n), space O(n)
+    public static int evalRPNArray(String[] tokens) {
+        int[] stack = new int[tokens.length];
+        int top = -1;
+        for (String op : tokens) {
+            switch (op) {
+                case "+":
+                    int first = stack[top--];
+                    int second = stack[top--];
+                    stack[++top] = first + second;
+                    break;
+                case "-":
+                    first = stack[top--];
+                    second = stack[top--];
+                    stack[++top] = second - first;
+                    break;
+                case "*":
+                    first = stack[top--];
+                    second = stack[top--];
+                    stack[++top] = first * second;
+                    break;
+                case "/":
+                    first = stack[top--];
+                    second = stack[top--];
+                    stack[++top] = second / first;
+                    break;
+                default:
+                    stack[++top] = Integer.parseInt(op);
+                    break;
+            }
+        }
+
+        return stack[top];
+    }
 }
--- src/test/java/com/solairerove/dsa/problems/P0150_EvaluateReversePolishNotationTest.java
+++ src/test/java/com/solairerove/dsa/problems/P0150_EvaluateReversePolishNotationTest.java
@@ -3,6 +3,7 @@ package com.solairerove.dsa.problems;
 import org.junit.jupiter.api.Test;
 
 import static com.solairerove.dsa.problems.P0150_EvaluateReversePolishNotation.evalRPN;
+import static com.solairerove.dsa.problems.P0150_EvaluateReversePolishNotation.evalRPNArray;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SuppressWarnings("NewClassNamingConvention")
@@ -63,5 +64,62 @@ class P0150_EvaluateReversePolishNotationTest {
     @Test
     void deeplyNestedExpression() {
         assertEquals(18, evalRPN(new String[]{"3", "4", "+", "2", "*", "7", "/", "5", "*", "8", "+"}));
+    }
+
+    @Test
+    void arrayLeetCodeExampleOne() {
+        assertEquals(9, evalRPNArray(new String[]{"2", "1", "+", "3", "*"}));
+    }
+
+    @Test
+    void arrayLeetCodeExampleTwo() {
+        assertEquals(6, evalRPNArray(new String[]{"4", "13", "5", "/", "+"}));
+    }
+
+    @Test
+    void arrayLeetCodeExampleThree() {
+        assertEquals(22, evalRPNArray(new String[]{
+                "10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"}));
+    }
+
+    @Test
+    void arraySingleNumber() {
+        assertEquals(42, evalRPNArray(new String[]{"42"}));
+    }
+
+    @Test
+    void arraySingleNegativeNumber() {
+        assertEquals(-7, evalRPNArray(new String[]{"-7"}));
+    }
+
+    @Test
+    void arraySubtractionIsOrderSensitive() {
+        assertEquals(-1, evalRPNArray(new String[]{"2", "3", "-"}));
+    }
+
+    @Test
+    void arrayDivisionIsOrderSensitive() {
+        assertEquals(0, evalRPNArray(new String[]{"3", "6", "/"}));
+    }
+
+    @Test
+    void arrayDivisionTruncatesTowardZero() {
+        assertEquals(-2, evalRPNArray(new String[]{"-7", "3", "/"}));
+        assertEquals(2, evalRPNArray(new String[]{"7", "3", "/"}));
+    }
+
+    @Test
+    void arrayMultiplicationWithNegative() {
+        assertEquals(-12, evalRPNArray(new String[]{"4", "-3", "*"}));
+    }
+
+    @Test
+    void arrayAllOperatorsChained() {
+        assertEquals(5, evalRPNArray(new String[]{"1", "2", "+", "9", "*", "3", "/", "4", "-"}));
+    }
+
+    @Test
+    void arrayDeeplyNestedExpression() {
+        assertEquals(18, evalRPNArray(new String[]{"3", "4", "+", "2", "*", "7", "/", "5", "*", "8", "+"}));
     }
 }
