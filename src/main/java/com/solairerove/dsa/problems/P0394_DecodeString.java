b7cb9f7dbafa1b65aca2ee2792918e7e59797249
solairerove <solairerove@gmail.com>
2026-09-10 16:09:25 +0300

    Add `decodeStringTwoStacks` implementation with O(n * k) time complexity and comprehensive test coverage

--- src/main/java/com/solairerove/dsa/problems/P0394_DecodeString.java
+++ src/main/java/com/solairerove/dsa/problems/P0394_DecodeString.java
@@ -35,4 +35,31 @@ public class P0394_DecodeString {
 
         return res.toString();
     }
+
+    // time O(n * k), space O(n)
+    public static String decodeStringTwoStacks(String s) {
+        Stack<String> stringStack = new Stack<>();
+        Stack<Integer> countStack = new Stack<>();
+        StringBuilder curr = new StringBuilder();
+        int k = 0;
+        for (char ch : s.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                k = k * 10 + (ch - '0');
+            } else if (ch == '[') {
+                stringStack.push(curr.toString());
+                countStack.push(k);
+                curr = new StringBuilder();
+                k = 0;
+            } else if (ch == ']') {
+                String temp = curr.toString();
+                curr = new StringBuilder(stringStack.pop());
+                int count = countStack.pop();
+                curr.repeat(temp, Math.max(0, count));
+            } else {
+                curr.append(ch);
+            }
+        }
+
+        return curr.toString();
+    }
 }
--- src/test/java/com/solairerove/dsa/problems/P0394_DecodeStringTest.java
+++ src/test/java/com/solairerove/dsa/problems/P0394_DecodeStringTest.java
@@ -3,6 +3,7 @@ package com.solairerove.dsa.problems;
 import org.junit.jupiter.api.Test;
 
 import static com.solairerove.dsa.problems.P0394_DecodeString.decodeString;
+import static com.solairerove.dsa.problems.P0394_DecodeString.decodeStringTwoStacks;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SuppressWarnings("NewClassNamingConvention")
@@ -81,5 +82,80 @@ class P0394_DecodeStringTest {
     @Test
     void deepNestingWithSuffix() {
         assertEquals("accaccaccb", decodeString("3[a2[c]]b"));
+    }
+
+    @Test
+    void twoStacksLeetcodeExampleOne() {
+        assertEquals("aaabcbc", decodeStringTwoStacks("3[a]2[bc]"));
+    }
+
+    @Test
+    void twoStacksLeetcodeExampleTwo() {
+        assertEquals("accaccacc", decodeStringTwoStacks("3[a2[c]]"));
+    }
+
+    @Test
+    void twoStacksLeetcodeExampleThree() {
+        assertEquals("abcabccdcdcdef", decodeStringTwoStacks("2[abc]3[cd]ef"));
+    }
+
+    @Test
+    void twoStacksNoEncoding() {
+        assertEquals("abc", decodeStringTwoStacks("abc"));
+    }
+
+    @Test
+    void twoStacksEmptyInput() {
+        assertEquals("", decodeStringTwoStacks(""));
+    }
+
+    @Test
+    void twoStacksSingleRepeat() {
+        assertEquals("x", decodeStringTwoStacks("1[x]"));
+    }
+
+    @Test
+    void twoStacksMultiDigitCount() {
+        assertEquals("aaaaaaaaaa", decodeStringTwoStacks("10[a]"));
+    }
+
+    @Test
+    void twoStacksTripleNesting() {
+        assertEquals("aaaaaaaa", decodeStringTwoStacks("2[2[2[a]]]"));
+    }
+
+    @Test
+    void twoStacksLiteralBeforeAndAfter() {
+        assertEquals("abbc", decodeStringTwoStacks("a2[b]c"));
+    }
+
+    @Test
+    void twoStacksTrailingLiteral() {
+        assertEquals("aaab", decodeStringTwoStacks("3[a]b"));
+    }
+
+    @Test
+    void twoStacksNestedAfterLiteralInside() {
+        assertEquals("abcccabccc", decodeStringTwoStacks("2[ab3[c]]"));
+    }
+
+    @Test
+    void twoStacksNestedWithMultiDigitCounts() {
+        assertEquals("ab".repeat(12), decodeStringTwoStacks("3[4[ab]]"));
+    }
+
+    @Test
+    void twoStacksLargeCount() {
+        assertEquals("leetcode".repeat(100), decodeStringTwoStacks("100[leetcode]"));
+    }
+
+    @Test
+    void twoStacksTwoNestedSiblings() {
+        assertEquals("xyyxyyzz", decodeStringTwoStacks("2[x2[y]]2[z]"));
+    }
+
+    @Test
+    void twoStacksDeepNestingWithSuffix() {
+        assertEquals("accaccaccb", decodeStringTwoStacks("3[a2[c]]b"));
     }
 }
