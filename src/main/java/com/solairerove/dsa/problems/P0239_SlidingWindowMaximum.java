d7d79aa9498e56ca85031fd55405d20829bf61ef
solairerove <solairerove@gmail.com>
2026-09-17 09:33:52 +0300

    Replace `maxSlidingWindow` with `maxSlidingWindowHeap` in tests and refactor heap-based implementation for clarity and consistency

--- src/main/java/com/solairerove/dsa/problems/P0239_SlidingWindowMaximum.java
+++ src/main/java/com/solairerove/dsa/problems/P0239_SlidingWindowMaximum.java
@@ -1,41 +1,44 @@
 package com.solairerove.dsa.problems;
 
+import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.LinkedList;
 import java.util.PriorityQueue;
 
 public class P0239_SlidingWindowMaximum {
 
-    // time O(n * log(n)), space O(n)
-    public static int[] maxSlidingWindow(int[] nums, int k) {
+    // time O(n), space O(k)
+    public static int[] maxSlidingWindowDeque(int[] nums, int k) {
         int n = nums.length;
-        PriorityQueue<int[]> heap = new PriorityQueue<>((a, b) -> b[0] - a[0]);
-        int[] res = new int[n - k + 1];
 
-        int idx = 0;
+        Deque<Integer> dq = new ArrayDeque<>();
+        int[] res = new int[n - k + 1];
         for (int i = 0; i < n; i++) {
-            heap.offer(new int[]{nums[i], i});
-            if (i >= k - 1) {
-                while (heap.peek()[1] <= i - k) heap.poll();
-                res[idx++] = heap.peek()[0];
+            while (!dq.isEmpty() && dq.peekFirst() < i - k + 1) dq.pollFirst();
+
+            while (!dq.isEmpty() && nums[dq.peekLast()] < nums[i]) dq.pollLast();
+
+            dq.offerLast(i);
+            if (i - k + 1 >= 0) {
+                res[i - k + 1] = nums[dq.peekFirst()];
             }
         }
 
         return res;
     }
 
-    // time O(n), space O(k)
-    public static int[] maxSlidingWindowDeque(int[] nums, int k) {
+    // time O(n * log(n)), space O(n)
+    public static int[] maxSlidingWindowHeap(int[] nums, int k) {
         int n = nums.length;
+
+        PriorityQueue<int[]> heap = new PriorityQueue<>((a, b) -> b[0] - a[0]);
         int[] res = new int[n - k + 1];
-        Deque<Integer> dq = new LinkedList<>();
-
         for (int i = 0; i < n; i++) {
-            while (!dq.isEmpty() && dq.peekFirst() <= i - k) dq.pollFirst();
-            while (!dq.isEmpty() && nums[dq.peekLast()] < nums[i]) dq.pollLast();
+            while (!heap.isEmpty() && heap.peek()[1] < i - k + 1) heap.poll();
 
-            dq.offerLast(i);
-            if (i >= k - 1) res[i - k + 1] = nums[dq.peekFirst()];
+            heap.offer(new int[] {nums[i], i});
+            if (i - k + 1 >= 0) {
+                res[i - k + 1] = heap.peek()[0];
+            }
         }
 
         return res;
--- src/test/java/com/solairerove/dsa/problems/P0239_SlidingWindowMaximumTest.java
+++ src/test/java/com/solairerove/dsa/problems/P0239_SlidingWindowMaximumTest.java
@@ -2,7 +2,7 @@ package com.solairerove.dsa.problems;
 
 import org.junit.jupiter.api.Test;
 
-import static com.solairerove.dsa.problems.P0239_SlidingWindowMaximum.maxSlidingWindow;
+import static com.solairerove.dsa.problems.P0239_SlidingWindowMaximum.maxSlidingWindowHeap;
 import static com.solairerove.dsa.problems.P0239_SlidingWindowMaximum.maxSlidingWindowDeque;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
@@ -11,57 +11,57 @@ class P0239_SlidingWindowMaximumTest {
 
     @Test
     void example() {
-        assertArrayEquals(new int[]{3, 3, 5, 5, 6, 7}, maxSlidingWindow(new int[]{1, 3, -1, -3, 5, 3, 6, 7}, 3));
+        assertArrayEquals(new int[]{3, 3, 5, 5, 6, 7}, maxSlidingWindowHeap(new int[]{1, 3, -1, -3, 5, 3, 6, 7}, 3));
     }
 
     @Test
     void singleElement() {
-        assertArrayEquals(new int[]{1}, maxSlidingWindow(new int[]{1}, 1));
+        assertArrayEquals(new int[]{1}, maxSlidingWindowHeap(new int[]{1}, 1));
     }
 
     @Test
     void windowOfOne() {
-        assertArrayEquals(new int[]{4, 2, 7, 1}, maxSlidingWindow(new int[]{4, 2, 7, 1}, 1));
+        assertArrayEquals(new int[]{4, 2, 7, 1}, maxSlidingWindowHeap(new int[]{4, 2, 7, 1}, 1));
     }
 
     @Test
     void windowEqualsLength() {
-        assertArrayEquals(new int[]{9}, maxSlidingWindow(new int[]{3, 9, 2, 5}, 4));
+        assertArrayEquals(new int[]{9}, maxSlidingWindowHeap(new int[]{3, 9, 2, 5}, 4));
     }
 
     @Test
     void allEqual() {
-        assertArrayEquals(new int[]{2, 2, 2}, maxSlidingWindow(new int[]{2, 2, 2, 2, 2}, 3));
+        assertArrayEquals(new int[]{2, 2, 2}, maxSlidingWindowHeap(new int[]{2, 2, 2, 2, 2}, 3));
     }
 
     @Test
     void strictlyIncreasing() {
-        assertArrayEquals(new int[]{2, 3, 4, 5}, maxSlidingWindow(new int[]{1, 2, 3, 4, 5}, 2));
+        assertArrayEquals(new int[]{2, 3, 4, 5}, maxSlidingWindowHeap(new int[]{1, 2, 3, 4, 5}, 2));
     }
 
     @Test
     void strictlyDecreasing() {
-        assertArrayEquals(new int[]{5, 4, 3, 2}, maxSlidingWindow(new int[]{5, 4, 3, 2, 1}, 2));
+        assertArrayEquals(new int[]{5, 4, 3, 2}, maxSlidingWindowHeap(new int[]{5, 4, 3, 2, 1}, 2));
     }
 
     @Test
     void allNegative() {
-        assertArrayEquals(new int[]{-2, -2, -3}, maxSlidingWindow(new int[]{-5, -2, -3, -4}, 2));
+        assertArrayEquals(new int[]{-2, -2, -3}, maxSlidingWindowHeap(new int[]{-5, -2, -3, -4}, 2));
     }
 
     @Test
     void maxLeavesWindow() {
-        assertArrayEquals(new int[]{9, 3, 3}, maxSlidingWindow(new int[]{9, 1, 3, 2}, 2));
+        assertArrayEquals(new int[]{9, 3, 3}, maxSlidingWindowHeap(new int[]{9, 1, 3, 2}, 2));
     }
 
     @Test
     void duplicatesAtWindowEdge() {
-        assertArrayEquals(new int[]{7, 7, 7, 7}, maxSlidingWindow(new int[]{7, 1, 7, 1, 7}, 2));
+        assertArrayEquals(new int[]{7, 7, 7, 7}, maxSlidingWindowHeap(new int[]{7, 1, 7, 1, 7}, 2));
     }
 
     @Test
     void twoElements() {
-        assertArrayEquals(new int[]{1}, maxSlidingWindow(new int[]{1, -1}, 2));
+        assertArrayEquals(new int[]{1}, maxSlidingWindowHeap(new int[]{1, -1}, 2));
     }
 
     @Test
