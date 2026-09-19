2c51c60d305dcbfec57f345d1c61648e81fcfbab
solairerove <solairerove@gmail.com>
2026-09-19 12:21:27 +0300

    Refactor `P0394_DecodeString` to remove `Deque`-based implementation and streamline `Stack`-based solution

--- src/main/java/com/solairerove/dsa/problems/P0394_DecodeString.java
+++ src/main/java/com/solairerove/dsa/problems/P0394_DecodeString.java
@@ -1,70 +1,32 @@
 package com.solairerove.dsa.problems;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
 import java.util.Stack;
 
 public class P0394_DecodeString {
 
     // time O(n * k), space O(n)
     public static String decodeString(String s) {
-        Stack<String> dq = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == ']') {
+        Stack<String> stack = new Stack<>();
+        for (Character ch : s.toCharArray()) {
+            if (ch == ']') {
                 StringBuilder inner = new StringBuilder();
-                while (!dq.isEmpty() && !dq.peek().equals("[")) {
-                    inner.insert(0, dq.pop());
+                while (!stack.isEmpty() && Character.isLetter(stack.peek().toCharArray()[0])) {
+                    inner.insert(0, stack.pop());
                 }
-                dq.pop();
+                stack.pop();
 
-                StringBuilder k = new StringBuilder();
-                while (!dq.isEmpty() && Character.isDigit(dq.peek().charAt(0))) {
-                    k.insert(0, dq.pop());
+                StringBuilder count = new StringBuilder();
+                while (!stack.isEmpty() && Character.isDigit(stack.peek().toCharArray()[0])) {
+                    count.insert(0, stack.pop());
                 }
-                int count = Integer.parseInt(k.toString());
-                String repeatedStr = inner.toString().repeat(count);
 
-                dq.push(repeatedStr);
+                String substring = inner.toString().repeat(Integer.parseInt(count.toString()));
+                stack.push(substring);
             } else {
-                dq.push(String.valueOf(s.charAt(i)));
+                stack.push(String.valueOf(ch));
             }
         }
 
-        StringBuilder res = new StringBuilder();
-        while (!dq.isEmpty()) {
-            res.insert(0, dq.pop());
-        }
-
-        return res.toString();
-    }
-
-    private record Frame(StringBuilder prefix, int count) {
-    }
-
-    // time O(n * k), space O(n)
-    public static String decodeStringDeque(String s) {
-        Deque<Frame> dq = new ArrayDeque<>();
-        StringBuilder cur = new StringBuilder();
-        int count = 0;
-
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if (ch >= '0' && ch <= '9') {
-                count = count * 10 + (ch - '0');
-            } else if (ch == '[') {
-                dq.push(new Frame(cur, count));
-                cur = new StringBuilder();
-                count = 0;
-            } else if (ch == ']') {
-                Frame frame = dq.pop();
-                StringBuilder prefix = frame.prefix();
-                prefix.repeat(String.valueOf(cur), Math.max(0, frame.count()));
-                cur = prefix;
-            } else {
-                cur.append(ch);
-            }
-        }
-
-        return cur.toString();
+        return String.join("", stack);
     }
 }
--- src/test/java/com/solairerove/dsa/problems/P0394_DecodeStringTest.java
+++ src/test/java/com/solairerove/dsa/problems/P0394_DecodeStringTest.java
@@ -3,7 +3,6 @@ package com.solairerove.dsa.problems;
 import org.junit.jupiter.api.Test;
 
 import static com.solairerove.dsa.problems.P0394_DecodeString.decodeString;
-import static com.solairerove.dsa.problems.P0394_DecodeString.decodeStringDeque;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SuppressWarnings("NewClassNamingConvention")
@@ -82,80 +81,5 @@ class P0394_DecodeStringTest {
     @Test
     void deepNestingWithSuffix() {
         assertEquals("accaccaccb", decodeString("3[a2[c]]b"));
-    }
-
-    @Test
-    void dequeLeetcodeExampleOne() {
-        assertEquals("aaabcbc", decodeStringDeque("3[a]2[bc]"));
-    }
-
-    @Test
-    void dequeLeetcodeExampleTwo() {
-        assertEquals("accaccacc", decodeStringDeque("3[a2[c]]"));
-    }
-
-    @Test
-    void dequeLeetcodeExampleThree() {
-        assertEquals("abcabccdcdcdef", decodeStringDeque("2[abc]3[cd]ef"));
-    }
-
-    @Test
-    void dequeNoEncoding() {
-        assertEquals("abc", decodeStringDeque("abc"));
-    }
-
-    @Test
-    void dequeEmptyInput() {
-        assertEquals("", decodeStringDeque(""));
-    }
-
-    @Test
-    void dequeSingleRepeat() {
-        assertEquals("x", decodeStringDeque("1[x]"));
-    }
-
-    @Test
-    void dequeMultiDigitCount() {
-        assertEquals("aaaaaaaaaa", decodeStringDeque("10[a]"));
-    }
-
-    @Test
-    void dequeTripleNesting() {
-        assertEquals("aaaaaaaa", decodeStringDeque("2[2[2[a]]]"));
-    }
-
-    @Test
-    void dequeLiteralBeforeAndAfter() {
-        assertEquals("abbc", decodeStringDeque("a2[b]c"));
-    }
-
-    @Test
-    void dequeTrailingLiteral() {
-        assertEquals("aaab", decodeStringDeque("3[a]b"));
-    }
-
-    @Test
-    void dequeNestedAfterLiteralInside() {
-        assertEquals("abcccabccc", decodeStringDeque("2[ab3[c]]"));
-    }
-
-    @Test
-    void dequeNestedWithMultiDigitCounts() {
-        assertEquals("ab".repeat(12), decodeStringDeque("3[4[ab]]"));
-    }
-
-    @Test
-    void dequeLargeCount() {
-        assertEquals("leetcode".repeat(100), decodeStringDeque("100[leetcode]"));
-    }
-
-    @Test
-    void dequeTwoNestedSiblings() {
-        assertEquals("xyyxyyzz", decodeStringDeque("2[x2[y]]2[z]"));
-    }
-
-    @Test
-    void dequeDeepNestingWithSuffix() {
-        assertEquals("accaccaccb", decodeStringDeque("3[a2[c]]b"));
     }
 }
